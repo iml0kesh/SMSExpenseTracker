468c777c48143f9d1088db93681bf72737521e27
iml0kesh <iamlokeshofficial@gmail.com>
2026-01-02 12:58:17 +0530

    Ui clean up and come changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.smsexpensetracker"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
